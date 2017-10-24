2ed0b9b0876278e94986c8e0da434c8fdede468d
nissy34 <33037628+nissy34@users.noreply.github.com>
2017-10-25 00:36:46 +0300

    Initial commit

---
+++ BL/BL_imp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+using DAL;
+
+namespace BL
+{
+    class BL_imp:IBl
+    {
+        IDal dal = Factory_Dal.GetInstance_List();
+
+        public IEnumerable<Flower> GetAllFLowersByName(string name)
+        {
+            IEnumerable<Flower> flowers = dal.GetFlowerEnumerator();
+
+            return flowers.Where(flower => flower.Name.Contains(name));
+            
+        }//
+    }
+}
---
+++ DAL/Dal_imp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+
+namespace DAL
+{
+   public class Dal_imp : IDal
+    {
+        private readonly List<Flower> FloweraList;
+
+        public Dal_imp()
+        {
+
+            FloweraList=new List<Flower>();
+            FloweraList = new List<Flower>();
+            for (int i = 0; i < 15; i++)
+            {
+                FloweraList.Add(new Flower(i.ToString(), i.ToString(), i.ToString(), i.ToString(), i.ToString()));
+            }
+        }//
+        public IEnumerable<Flower> GetFlowerEnumerator()
+        {
+            return FloweraList.AsEnumerable();
+        }
+    }
+}
